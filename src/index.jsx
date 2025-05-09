cc700bac27351a8a08796dbe61e5026c1fe58855
sandesh <sandeshbhatta218@gmail.com>
2025-05-09 12:45:32 +0545

    Creating render function.

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import * as THREE from "three";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render();
