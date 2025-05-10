dfcd88c2e77a57da42a4c2769905ce5ab4e01c87
sandesh <sandeshbhatta218@gmail.com>
2025-05-10 10:50:54 +0545

    Comparing native Three and R3F.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,24 @@
+import { color } from "three/tsl";
 import "./style.css";
 import ReactDOM from "react-dom/client";
+import { BoxGeometry } from "three";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(<>Soon to be a badass R3F application</>);
+root.render(
+  <>
+    <mesh>
+      <boxGeometry />
+      <meshBasicMaterial color="red" />
+    </mesh>
+  </>
+);
+
+// Usual three for creating scene
+
+const mesh = new THREE.Mesh();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasciMaterial({ color: red });
+scene.add(mesh);
+
+// Creating mesh in R3F
