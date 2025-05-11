eca8bf6a92c22715a6bf0501b0128cd3e68aac89
sandesh <sandeshbhatta218@gmail.com>
2025-05-11 10:48:49 +0545

    Implementing basics of R3F.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,18 @@
 export default function Experience() {
   return (
     <>
-      <mesh>
+      <mesh position-x={-2}>
         <sphereGeometry />
-        <meshBasicMaterial />
+        <meshBasicMaterial color="orange" />
+      </mesh>
+      <mesh rotation-y={[Math.PI * 0.25]} position-x={2} scale={1.5}>
+        {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
+        <boxGeometry />
+        <meshBasicMaterial color="green" wireframe="false" />
+      </mesh>
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshBasicMaterial color="blue" />
       </mesh>
     </>
   );
