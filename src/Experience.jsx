e7251e9070ea567213eaa27bfe9459f2ef7af2a5
sandesh <sandeshbhatta218@gmail.com>
2025-05-12 10:20:15 +0545

    Importing useFrame for animating the scene.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,11 @@
+import { useFrame } from "@react-three/fiber";
+
 export default function Experience() {
+  useFrame(() => {});
+
   return (
     <>
+      {/* Creating Scenes */}
       <mesh position-x={-2}>
         <sphereGeometry />
         <meshBasicMaterial color="orange" />
@@ -8,12 +13,14 @@ export default function Experience() {
       <mesh rotation-y={[Math.PI * 0.25]} position-x={2} scale={1.5}>
         {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
         <boxGeometry />
-        <meshBasicMaterial color="green" wireframe="false" />
+        <meshBasicMaterial color="purple" wireframe="false" />
       </mesh>
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshBasicMaterial color="blue" />
+        <meshBasicMaterial color="green" />
       </mesh>
     </>
   );
 }
+
+// Using useFrame hook provided by R3F for animating scene.
