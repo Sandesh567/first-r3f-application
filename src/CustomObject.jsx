4dd69f03fb606586f93d3298666ebd23ef91cb5f
sandesh <sandeshbhatta218@gmail.com>
2025-05-14 09:17:40 +0545

    Creating Random shapes using Float32Array and looping to create random triangle.

--- src/CustomObject.jsx
+++ src/CustomObject.jsx
@@ -1,8 +1,27 @@
+import * as THREE from "three";
+
 export default function CustomObject() {
+  const verticesCount = 10 * 3;
+  const positions = new Float32Array(verticesCount * 3);
+
+  for (let i = 0; i < verticesCount * 3; i++)
+    positions[i] = (Math.random() - 0.5) * 3;
+
   return (
     <mesh>
-      <boxGeometry />
-      <meshBasicMaterial color="red" wireframe={true} />
+      <bufferGeometry>
+        <bufferAttribute
+          attach="attributes-position"
+          count={verticesCount}
+          itemSize={3}
+          array={positions}
+        />
+      </bufferGeometry>
+      <meshBasicMaterial
+        color="red"
+        wireframe={false}
+        side={THREE.DoubleSide}
+      />
     </mesh>
   );
 }
